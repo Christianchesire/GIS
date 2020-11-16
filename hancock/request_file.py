5f2ee3b0c5cf3bb14bfce0e1310a95abc64076ad
Christianchesire <christian.chesire@strathmore.edu>
2020-11-16 18:04:52 -0500

    Using the requeat module

---
+++ hancock/request_file.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://github.com/GeospatialPython/Learning/raw/master/hancock.zip"
+
+fileName = "hancock.zip"
+
+r = requests.get(url)
+
+with open(fileName, 'wb') as f:
+   f.write(r.content)
